2b70ab907c56415f1db504712c582e4f06bddb37
Aviel Aldama <teku-acceledrom-bass@live.com.mx>
2017-06-06 23:23:47 -0500

    Estructura Base, JWSR CrossPlatform

---
+++ JWService/JWSRAndroid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace JWSRAndroid
+{
+    [Activity(Label = "JWSRAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
